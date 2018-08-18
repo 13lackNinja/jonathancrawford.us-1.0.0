2de8e90fce215a9d817c42947b8e6ec18ea5ab1c
13lackninja <jonathan.crawford55@gmail.com>
2018-08-17 21:02:46 -0700

    added moodboard

--- jonathancrawford.js
+++ jonathancrawford.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
   res.sendFile('/markup/index.html', { root: './public' });
 });
 
+app.get('/moodboard', (req, res) => {
+  res.sendFile('/markup/moodboard.html', { root: './public' });
+});
+
 // Port
 
 app.listen(9000);
